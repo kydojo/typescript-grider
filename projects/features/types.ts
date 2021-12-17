421651aa2deecca1702af7b319f6151904c067f7
Kyle Johnson <kylejohnson2@gamestop.com>
2021-12-17 16:47:24 -0600

    fetchjson (with axios) and types examples

---
+++ projects/features/types.ts
@@ -0,0 +1,14 @@
+// Hover over the vars to see the types they're pointing to
+
+const today = new Date();
+const month = today.getMonth();
+
+const person = {
+  age: 20
+};
+
+class Color {
+  
+}
+
+const red = new Color();
